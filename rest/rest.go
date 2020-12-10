838627227eaeea70c0e6c8238b1aa5b320b8e661
qoxogus <a01034367055@gmail.com>
2020-12-10 20:52:41 +0900

    update

--- rest/rest.go
+++ rest/rest.go
@@ -7,12 +7,15 @@ import (
 
 //RunAPI used in main.go
 func RunAPI(address string) {
-	tmp := "C:/Users/user/go/src/Gsmfestival-Master"
+	tmp := "C:/Users/user/go/src/Gsmfestival-Master-Front"
 	e := echo.New()
 	// e.Use(middleware.Logger())
+	e.Static("/", "static")
 	e.Use(middleware.Static(tmp))
-	e.GET("/", GetMainPage)          //서버 검사
-	e.POST("/signupsuccess", Signup) //회원정보 insert
-	// e.POST("/signin", Signin)
+	e.GET("/", GetMainPage)      //서버 검사
+	e.POST("/loginpage", Signup) //회원정보 insert
+	e.POST("/mainpage", Signin)
 	e.Logger.Fatal(e.Start(address))
 }
+
+//배태현
