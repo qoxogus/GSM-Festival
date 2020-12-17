f9612b38fa394b3e9aa2605a7298c87cabefa977
qoxogus <a01034367055@gmail.com>
2020-12-17 20:35:32 +0900

    add main.go dbdonn

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/qoxogus/GSM-Festival-Master-Back/database"
 	"github.com/qoxogus/GSM-Festival-Master-Back/rest"
 )
 
 func main() {
+
 	rest.RunAPI(":1324")
+	database.Connect()
 }
